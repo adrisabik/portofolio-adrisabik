68db3fcb542003b2a5f240fefa2ee033fc3b5b2b
adrisabik <adrisabik@gmail.com>
2026-01-18 19:44:40 +0700

    chore(next): configure next.config for Velite integration

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  // Disable webpack extension for now as Velite is processed in prebuild
-  /*
-  webpack: (config) => {
-    return config;
-  },
-  */
 };
 
 export default nextConfig;
